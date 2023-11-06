00215db1758d8bf7307d7e21d8d6cf1fa604c2de
Matheus Avelino <avelinomatheus2703@gmail.com>
2023-11-05 21:10:15 -0300

    criando repositorio dia 6

--- dia 5/index.js
+++ dia 5/index.js
@@ -1,3 +1,11 @@
+//1 - Calculadora DoDev
+
+//1.1 - Crie um algoritmo que peça 2 números ao usuário
+
+//1.2 - Utilizando o switchCase dê a opção do usuário escolher qual operação matemática será realizada com esses números
+
+//1.3 - Exiba no console a operação feita e o resultado da conta
+
 let num1 = parseInt(prompt("Digite um numero"));
 let num2 = parseInt(prompt("Digite outro numero"));
 let opcao1 = parseInt(
@@ -23,6 +31,18 @@ switch (opcao) {
 }
 
 //SEGUNDO EXEMPLO
+
+//DESAFIO DIA #5:
+
+//2 - DoDev thru
+
+//1.1 - Crie um algoritmo que peça para o usuário escolher entre 3 opções em um posto de gasolina: abastecer com gasolina, com álcool ou calibrar os pneus.
+
+//1.2 - Se o usuário escolher abastecer com gasolina, você deve solicitar o valor desejado e exibir no console a quantidade em litros que foi abastecida (litro de gasolina = R$5)
+
+//1.3 - Se o usuário escolher abastecer com álcool, você deve solicitar o valor desejado e exibir no console a quantidade em litros que foi abastecida (litro de álcool = R$3)
+
+//1.4 - Se o usuário escolher calibrar os pneus, exiba no console a mensagem: "pneus calibrados com sucesso".
 let valor;
 let quantidade;
 let opcao = prompt(
---
+++ dia 6/index.js
@@ -0,0 +1,29 @@
+//Exercicio 1
+
+//let numero = Number(prompt("escolha um numero"));
+
+//for (let i = 0; i <= numero; i++) {
+// console.log(`a contagem esta no ${i}`);
+//}
+
+//exercicio 2
+//Desenvolva um algoritmo que usando a estrutura FOR mostre no console uma
+//contagem de 0 a 50, porém, essa contagem tem que ser feita de 5 em 5.
+
+//for (let i = 0; i <= 50; i += 5) {
+// console.log(i);
+//}
+
+//exercicio 3
+//Agora faça com que o algoritmo do
+//exercício anterior exiba a mesma contagem mas na ordem inversa.
+
+//for (let i = 50; i >= 0; i -= 5) {
+// console.log(i);
+//}
+
+//let numero = Number(prompt("escolha um numero"));
+
+//for (let i = 0; i <= numero * 10; i += numero) {
+//console.log(i);
+//}
